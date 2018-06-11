c4c09855ebafcc4e46c535514e01a19183e2edaf
Nathalie Jonsson <jonsson.nathalie@gmail.com>
2018-06-11 16:09:30 +0100

    removed console logs and hidden number longer than the display

--- calculator.js
+++ calculator.js
@@ -1,55 +1,40 @@
-console.log(`this is a calculator!`);
-
 const input = document.getElementById('input');
 const tot = document.getElementById('tot');
 const clear = document.getElementById('clear');
 const equal = document.getElementById('equals');
 const buttons = document.querySelectorAll('button');
-console.log(buttons);
 
 let total = 0;
 let sumArr = [];
 let sum = 0;
-//let typed = '';
-
 
 buttons.forEach(button => {
   button.addEventListener('click', function() {
-    // typed input for the display
-      //typed = this.innerText;
-      if (button.className === "output"){
-        let typed = this.innerText;
-        input.textContent += typed;
-      }
+      sumArr.push(this.innerText);
+      sumArr.join("");
 
-        sumArr.push(this.innerText);
-        console.log(sumArr);
-        sumArr.join("");
+    if (button.className === "output"){
+       let typed = this.innerText;
+      input.textContent += typed;
+    }
+
       if (button.getAttribute("target") === "calc") {
-        console.log('you clicked an operator');
         if (sumArr[sumArr.length-2] === "+"||  sumArr[sumArr.length-2] === "-"
          || sumArr[sumArr.length-2] === "*" || sumArr[sumArr.length-2] === "/") {
           input.textContent = 'error: you clicked an operator twice';
         }
       } else if (button.id  === "decimal") {
-        console.log('you clicked the decimal');
         if (sumArr[sumArr.length-2] === ".") {
           input.textContent = 'error: you clicked the decimal twice';
         }
       } else if (button.id === "equals") {
-        console.log('you clicked the equal sign');
         sumArr.pop(sumArr.length-1)
         sum = eval(sumArr.join(""))
-        console.log('this is sum: ' + sum)
         tot.innerText = sum;
-        //console.log('this is sum: ' + sum);
       } else if (button.id === "clear") {
-        console.log('you clicked the clear button');
         input.textContent = '';
-        //sumArr.pop(sumArr.length-1)
         sumArr = [];
         tot.textContent = 0;
-        //sum = parseFloat(0.0);
       }
 
   });
